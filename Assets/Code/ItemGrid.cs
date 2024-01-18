6244363493614ca4e1fb02fad0000d8a229a7451
Le-gars-des-vues <102174064+Le-gars-des-vues@users.noreply.github.com>
2024-01-18 16:45:16 -0500

    Corriger Bug Inventaire

--- Assets/Code/ItemGrid.cs
+++ Assets/Code/ItemGrid.cs
@@ -6,8 +6,8 @@ public class ItemGrid : MonoBehaviour
 {
     [Header("Tile Format")]
     //Faut somehow mentir au code pour qu'il marche
-    const float tileSizeWidth = 16;
-    const float tileSizeHeight = 16;
+    const float tileSizeWidth = 32;
+    const float tileSizeHeight = 32;
 
     [Header("Transform of Grid")]
     RectTransform rectTransform;
@@ -27,22 +27,25 @@ public class ItemGrid : MonoBehaviour
         Init(gridSizeWidth, gridSizeHeight);
     }
 
+    //Initialise la grille a la taille desire selon les parametres gridSizeWitdh/Height
     private void Init(int width, int height)
     {
+        //Cree autant de case dans le array que de case sur la grille
         inventoryItemSlot = new InventoryItem[width, height];
-        Vector2 size= new Vector2(width*tileSizeWidth*2, height*tileSizeHeight*2);
+        //Multiplie le nombre de case par leur taille
+        Vector2 size= new Vector2(width*tileSizeWidth, height*tileSizeHeight);
         rectTransform.sizeDelta = size;
     }
     
     public Vector2Int GetTileGridPosition(Vector2 mousePosition)
     {
-        //Bug SOMEWHERE
+        //Calcule la difference entre la position de la souris et le pivot du RectTransform (la case en haut a droite devrait etre 0,0)
         positionOnTheGrid.x = mousePosition.x-rectTransform.position.x;
         positionOnTheGrid.y = rectTransform.position.y - mousePosition.y;
 
-        //Le diviser par 4 est la a cause du 0.25 scale sur la texture live
-        tileGridPosition.x = (int)(positionOnTheGrid.x / tileSizeWidth)/4;
-        tileGridPosition.y = (int)(positionOnTheGrid.y / tileSizeHeight)/4;
+        //Transform la position en Int pour identifier la bonne case. Le second diviseur est en fonction du Scale du RectTransform, pas besoin de changer le code si on change le scale!
+        tileGridPosition.x = (int)(positionOnTheGrid.x / tileSizeWidth) / (int)rectTransform.localScale.x;
+        tileGridPosition.y = (int)(positionOnTheGrid.y / tileSizeHeight) / (int)rectTransform.localScale.y;
 
         return tileGridPosition;
     }
