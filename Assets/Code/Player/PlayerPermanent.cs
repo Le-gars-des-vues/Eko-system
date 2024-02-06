9369e17e961ce932ffe6e397e9b1db605817a63a
Le-gars-des-vues <102174064+Le-gars-des-vues@users.noreply.github.com>
2024-02-05 21:50:03 -0500

    Forest jouable

--- Assets/Code/Player/PlayerPermanent.cs
+++ Assets/Code/Player/PlayerPermanent.cs
@@ -71,10 +71,12 @@ public class PlayerPermanent : MonoBehaviour
     {
         ResetToMax();
         ToggleRagdoll(false);
+        /*
         playerInventory.GetComponent<Image>().color = new Color(255, 255, 255, 0);
         storageInventory.GetComponent<Image>().color = new Color(255, 255, 255, 0);
         playerInventory.GetComponent<RectTransform>().localPosition = new Vector2(playerInventory.GetComponent<RectTransform>().localPosition.x, playerInventory.GetComponent<RectTransform>().localPosition.y - gridOffset);
         storageInventory.GetComponent<RectTransform>().localPosition = new Vector2(storageInventory.GetComponent<RectTransform>().localPosition.x, storageInventory.GetComponent<RectTransform>().localPosition.y - gridOffset);
+        */
         vineController = GetComponent<VinePlayerController>();
     }
 
