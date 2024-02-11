7a7fa695ced55ca50851b4b96039b2f7dd8bbb92
DuboisPascal <dubois98p@gmail.com>
2024-02-11 16:23:29 -0500

    HotbarGoesBrrrrr

--- Assets/Code/Inventaire/GridInterract.cs
+++ Assets/Code/Inventaire/GridInterract.cs
@@ -9,15 +9,34 @@ public class GridInterract : MonoBehaviour, IPointerEnterHandler, IPointerExitHa
 {
     InventoryController inventoryController;
     ItemGrid itemGrid;
+    [SerializeField] Transform canvasTransform;
+    RectTransform rectTransform;
 
     public void OnPointerEnter(PointerEventData eventData)
     {
         inventoryController.SelectedItemGrid = itemGrid;
+        if(inventoryController.selectedItem != null && inventoryController.SelectedItemGrid.gameObject.tag == "Hotbar")
+        {
+            inventoryController.selectedItem.itemData.height = inventoryController.selectedItem.itemData.hotbarHeight;
+            inventoryController.selectedItem.itemData.width = inventoryController.selectedItem.itemData.hotbarWidth;
+        }
+        if (inventoryController.selectedItem != null)
+        {
+            rectTransform = inventoryController.selectedItem.GetComponent<RectTransform>();
+            rectTransform.SetParent(canvasTransform);
+            rectTransform.SetAsLastSibling();
+        }
+        
     }
 
     public void OnPointerExit(PointerEventData eventData)
     {
         inventoryController.SelectedItemGrid=null;
+        if (inventoryController.selectedItem != null)
+        {
+            inventoryController.selectedItem.itemData.height = inventoryController.selectedItem.itemData.initialHeight;
+            inventoryController.selectedItem.itemData.width = inventoryController.selectedItem.itemData.initialWidth;
+        }
     }
 
     private void Awake()
