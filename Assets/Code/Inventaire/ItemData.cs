206cdcbfd567e49e44debcca653e13cbee4f7ee3
DuboisPascal <dubois98p@gmail.com>
2024-02-11 16:23:29 -0500

    HotbarGoesBrrrrr

--- Assets/Code/Inventaire/ItemData.cs
+++ Assets/Code/Inventaire/ItemData.cs
@@ -10,8 +10,22 @@ public class ItemData : ScriptableObject
     public int width=1;
     public int height = 1;
 
+    public int initialWidth=1;
+    public int initialHeight=1;
+
+    [HideInInspector]
+    public int hotbarWidth = 3;
+    [HideInInspector]
+    public int hotbarHeight = 3;
+
 
     public Sprite itemIcon;
 
     public int value=1;
+
+    private void OnEnable()
+    {
+        width = initialWidth;
+        height = initialHeight;
+    }
 }
