22098b95eb88b8d3480f61f027b3250b4ca24ef2
DuboisPascal <dubois98p@gmail.com>
2024-02-19 00:55:08 -0500

    slow start crafting

--- Assets/Code/Crafting/CraftingSystem.cs
+++ Assets/Code/Crafting/CraftingSystem.cs
@@ -1,18 +1,52 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class CraftingSystem : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    [SerializeField] private RectTransform inventaireCrafting;
 
-    // Update is called once per frame
-    void Update()
+    private int tempHeight;
+    private int tempWidth;
+    public float profit = 0;
+    ItemGrid theItemGrid;
+    InventoryItem anItem;
+
+    [SerializeField] private int quota;
+    [SerializeField] private TextMeshProUGUI quotaText;
+    // Start is called before the first frame update
+    public void VenteItem()
     {
-        
+        tempHeight = inventaireCrafting.GetComponent<ItemGrid>().GetGridSizeHeight();
+        tempWidth = inventaireCrafting.GetComponent<ItemGrid>().GetGridSizeWidth();
+
+        theItemGrid = inventaireCrafting.GetComponent<ItemGrid>();
+
+
+        for (int x = 0; x < tempWidth; x++)
+        {
+
+            for (int y = 0; y < tempHeight; y++)
+            {
+                //Not working
+                anItem = theItemGrid.CheckIfItemPresent(x, y);
+                if (anItem != null)
+                {
+                    profit += (float)anItem.itemData.value / (anItem.itemData.width * anItem.itemData.height);
+                    quotaText.text = profit.ToString() + " / " + quota.ToString();
+                    anItem.Delete();
+                    /*
+                    for (int ix = x; ix < x + anItem.itemData.width - 1; ix++)
+                    {
+                        for (int iy = x; iy < y + anItem.itemData.height - 1; iy++)
+                        {
+                            
+                        }
+                    }
+                    */
+                }
+            }
+        }
     }
 }
