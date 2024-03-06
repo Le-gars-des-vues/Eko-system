f30ed929587dd20367b1f885fca094005a4575ac
DuboisPascal <dubois98p@gmail.com>
2024-03-06 02:46:12 -0500

    Room System done
    needs debug

--- Assets/Code/Crafting/RoomCrafting.cs
+++ Assets/Code/Crafting/RoomCrafting.cs
@@ -32,7 +32,15 @@ public class RoomCrafting : MonoBehaviour
 
     // Start is called before the first frame update
 
+    public void SetFarm(int theFarms)
+    {
+        farms = theFarms;
+    }
 
+    public void SetEnclos(int theEnclos)
+    {
+        enclos = theEnclos;
+    }
     public int GetFarms()
     {
         return farms;
