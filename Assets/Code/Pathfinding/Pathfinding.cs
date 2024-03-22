c8a8b5b8d264035bab4ebf2afb9852d417c9e108
Le-gars-des-vues <102174064+Le-gars-des-vues@users.noreply.github.com>
2024-03-22 14:23:43 -0400

    More pathfinding fixes

--- Assets/Code/Pathfinding/Pathfinding.cs
+++ Assets/Code/Pathfinding/Pathfinding.cs
@@ -3,9 +3,11 @@ using System.Collections.Generic;
 using UnityEngine;
 using System;
 
-public class PathfindingDOTS : MonoBehaviour
+
+public class Pathfinding : MonoBehaviour
 {
     //Le grid utiliser pour le pathfinding
+    PathRequestManager requestManager;
     NodeGrid grid;
 
     //Les cout de mouvements de case
@@ -14,18 +16,25 @@ public class PathfindingDOTS : MonoBehaviour
 
     private void Awake()
     {
+        requestManager = GetComponent<PathRequestManager>();
         grid = GetComponent<NodeGrid>();
     }
 
+    public void StartFindPath(Vector2 startPos, Vector2 targetPos, bool isFlying)
+    {
+        StartCoroutine(FindPath(startPos, targetPos, isFlying));
+    }
+
     //Methode pour creer un path
-    public void FindPath(PathRequest request, Action<PathResult> callback, bool isFlying)
+    IEnumerator FindPath(Vector2 startPos, Vector2 endPos, bool isFlying)
     {
         Vector2[] waypoints = new Vector2[0];
         bool pathSuccess = false;
 
         //On definit les points de depart et d'arriver
-        PathNode startNode = grid.NodeFromWorldPoint(request.pathStart);
-        PathNode targetNode = grid.NodeFromWorldPoint(request.pathEnd);
+        PathNode startNode = grid.NodeFromWorldPoint(startPos);
+        PathNode targetNode = grid.NodeFromWorldPoint(endPos);
+        Debug.Log(startPos + " / " + endPos);
 
         if (startNode.isWalkable && targetNode.isWalkable)
         {
@@ -39,6 +48,7 @@ public class PathfindingDOTS : MonoBehaviour
             //Tant qu'il y a des cases a verifier
             while (openSet.Count > 0)
             {
+                Debug.Log("Checked node");
                 //On classe les nodes en ordre de cout de mouvement
                 PathNode currentNode = openSet.RemoveFirst();
                 //On met la node actuelle dans le hashset des cases deja verifier
@@ -81,7 +91,11 @@ public class PathfindingDOTS : MonoBehaviour
                 }
             }
         }
-
+        else
+        {
+            Debug.Log("Node are not walkable");
+        }
+        yield return null;
         if (pathSuccess)
         {
             waypoints = RetracePath(startNode, targetNode);
@@ -89,7 +103,7 @@ public class PathfindingDOTS : MonoBehaviour
         }
         else
             Debug.Log("Can't find path");
-        callback(new PathResult(waypoints, pathSuccess, request.callback));
+        requestManager.FinishedProcessingPath(waypoints, pathSuccess);
     }
 
     Vector2[] RetracePath(PathNode startNode, PathNode endNode)
@@ -137,4 +151,83 @@ public class PathfindingDOTS : MonoBehaviour
             return distanceX * MOVE_DIAGONAL + (distanceY - distanceX) * MOVE_HORIZONTAL;
         }
     }
+
+
+    /*
+    //Methode pour creer un path
+    public void FindPath(PathRequest request, Action<PathResult> callback)
+    {
+        Vector2[] waypoints = new Vector2[0];
+        bool pathSuccess = false;
+
+        //On definit les points de depart et d'arriver
+        PathNode startNode = grid.NodeFromWorldPoint(request.pathStart);
+        PathNode targetNode = grid.NodeFromWorldPoint(request.pathEnd);
+
+        if (startNode.isWalkable && targetNode.isWalkable)
+        {
+            //On stock les cases a verifier dans un heap
+            Heap<PathNode> openSet = new Heap<PathNode>(grid.MaxSize);
+            //On stock les cases deja verifier dans un hashset
+            HashSet<PathNode> closedSet = new HashSet<PathNode>();
+            //On ajoute la node de depart dans le heap des cases a verifier
+            openSet.Add(startNode);
+
+            //Tant qu'il y a des cases a verifier
+            while (openSet.Count > 0)
+            {
+                //On classe les nodes en ordre de cout de mouvement
+                PathNode currentNode = openSet.RemoveFirst();
+                //On met la node actuelle dans le hashset des cases deja verifier
+                closedSet.Add(currentNode);
+
+                //Si on a atteint la node finale
+                if (currentNode == targetNode)
+                {
+                    pathSuccess = true;
+                    //On trace le path
+                    break;
+                }
+
+                //Pour chaque case voisines
+                foreach (PathNode neighbour in grid.GetNeighbours(currentNode))
+                {
+                    //Si elle n'est pas walkable, on la skip
+                    if (!neighbour.isWalkable || closedSet.Contains(neighbour))
+                        continue;
+
+                    int newMoveCostToNeighbour;
+                    //On regarde le cout potentiel de la case voisine
+                    if (!request.isFlying)
+                        newMoveCostToNeighbour = currentNode.gCost + GetDistance(currentNode, neighbour) + neighbour.movementPenalty + neighbour.airPenalty;
+                    else
+                        newMoveCostToNeighbour = currentNode.gCost + GetDistance(currentNode, neighbour) + neighbour.movementPenalty;
+
+                    //Si celui-ci est plus plus bas ou qu'il n'est pas dans la liste des cases a verifier
+                    if (newMoveCostToNeighbour < neighbour.gCost || !openSet.Contains(neighbour))
+                    {
+                        neighbour.gCost = newMoveCostToNeighbour;
+                        neighbour.hCost = GetDistance(neighbour, targetNode);
+                        neighbour.cameFromNode = currentNode;
+
+                        if (!openSet.Contains(neighbour))
+                            openSet.Add(neighbour);
+                        else
+                            openSet.UpdateItem(neighbour);
+                    }
+                }
+            }
+        }
+
+        if (pathSuccess)
+        {
+            waypoints = RetracePath(startNode, targetNode);
+            pathSuccess = waypoints.Length > 0;
+        }
+        else
+            Debug.Log("Can't find path");
+        callback(new PathResult(waypoints, pathSuccess, request.callback));
+    }
+    */
 }
+
