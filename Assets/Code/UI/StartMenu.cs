2e00be432dee26c9b9c0616dd78f31ab2cd51f35
cwu03 <cwu0304@gmail.com>
2024-05-04 23:47:58 -0400

    MainMenu

--- Assets/Code/UI/StartMenu.cs
+++ Assets/Code/UI/StartMenu.cs
@@ -2,11 +2,15 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UIElements;
 
 public class StartMenu : MonoBehaviour
 {
+    [SerializeField] GameObject cresdits;
     // Start is called before the first frame update
     void Start(){
+        cresdits = transform.Find("cresdits").gameObject;
+        cresdits.SetActive(false);
     }
 
     public void PlayGame()
@@ -19,5 +23,15 @@ public class StartMenu : MonoBehaviour
     {
         Application.Quit();
         Debug.Log("Quit to Desktop");
+    }
+
+    public void ShowCredits()
+    {
+        cresdits.SetActive(true);
+    }
+
+    public void HideCredits()
+    {
+        cresdits.SetActive(false);
     }
 }
